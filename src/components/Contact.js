72a6dbdd7245a9af75976aa1ce03d10bc51e83e7
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-11-03 16:23:53 +0100

    feat: Add form validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import Footer from "./Footer";
 
 const Contact = () => {
-  const name = "The Digital Shop";
+  const name = "Olukunle Joseph/lerderl";
+  const navigate = useNavigate();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    navigate('/');
+  };
 
   return (
     <div className='container'>
@@ -11,23 +17,24 @@ const Contact = () => {
       <h1>Contact Me</h1>
       <p>Hi there, contact me to ask me about anything you have in mind.</p>
       {/* form */}
-      <form className="row g-3">
+      <form className="row g-3" onSubmit={handleSubmit}>
         <div className="col-md-6">
           <label htmlFor="first_name" className="form-label">First name</label>
-          <input type="text" className="form-control" id="first_name" placeholder='Enter your first name' />
+          <input type="text" className="form-control" id="first_name" placeholder='Enter your first name' required/>
         </div>
         <div className="col-md-6">
           <label htmlFor="last_name" className="form-label">Last name</label>
-          <input type="text" className="form-control" id="last_name" placeholder='Enter your last name' />
+          <input type="text" className="form-control" id="last_name" placeholder='Enter your last name' required/>
         </div>
         <div className="col-12">
           <label htmlFor="email" className="form-label">Email</label>
-          <input type="email" className="form-control" id="email" placeholder='yourname@email.com' />
+          <input type="email" className="form-control" id="email" placeholder='yourname@email.com' required/>
         </div>
         <div className="col-12">
           <label htmlFor="message" className="form-label">Message</label>
           <textarea
             rows={4}
+            required
             type="text"
             id="message"
             className="form-control"
@@ -36,7 +43,7 @@ const Contact = () => {
         </div>
         <div className="col-12">
           <div className="form-check">
-            <input className="form-check-input" type="checkbox" id="gridCheck" />
+            <input className="form-check-input" type="checkbox" id="gridCheck" required/>
             <label className="form-check-label" htmlFor="gridCheck">
               You agree to providing your data to {name} who may contact you.
             </label>
